bcbfa516988702a77afa423ff29e5ff5e075bc16
Yulia <yudemins@mail.ru>
2022-08-28 10:58:57 +0300

    fix: resolve other link fo cards

--- js/blue.js
+++ js/blue.js
@@ -1,84 +1,84 @@
 const cardsDataBlue = [
     {
       id: 'blue1',
-      url: '../assets/mythicCards/blue/blue1.jpg',
+      url: 'assets/mythicCards/blue/blue1.jpg',
     //   cardFace: blueCardsAssets.blue1,
       difficulty: 'hard',
       color:'blue'
     },
     {
       id: 'blue2',
-      url: '../assets/mythicCards/blue/blue2.jpg',
+      url: 'assets/mythicCards/blue/blue2.jpg',
     //   cardFace: blueCardsAssets.blue2,
       difficulty: 'hard',
       color:'blue'
     },
     {
       id: 'blue3',
-      url: '../assets/mythicCards/blue/blue3.jpg',
+      url: 'assets/mythicCards/blue/blue3.jpg',
     //   cardFace: blueCardsAssets.blue3,
       difficulty: 'easy',
       color:'blue'
     },
     {
       id: 'blue4',
-      url: '../assets/mythicCards/blue/blue4.jpg',
+      url: 'assets/mythicCards/blue/blue4.jpg',
     //   cardFace: blueCardsAssets.blue4,
       difficulty: 'easy',
       color:'blue'
     },
     {
       id: 'blue5',
-      url: '../assets/mythicCards/blue/blue5.jpg',
+      url: 'assets/mythicCards/blue/blue5.jpg',
     //   cardFace: blueCardsAssets.blue5,
       difficulty: 'easy',
       color:'blue'
     },
     {
       id: 'blue6',
-      url: '../assets/mythicCards/blue/blue6.jpg',
+      url: 'assets/mythicCards/blue/blue6.jpg',
     //   cardFace: blueCardsAssets.blue6,
       difficulty: 'hard',
       color:'blue'
     },
     {
       id: 'blue7',
-      url: '../assets/mythicCards/blue/blue7.jpg',
+      url: 'assets/mythicCards/blue/blue7.jpg',
     //   cardFace: blueCardsAssets.blue7,
       difficulty: 'normal',
       color:'blue'
     },
     {
       id: 'blue8',
-      url: '../assets/mythicCards/blue/blue8.jpg',
+      url: 'assets/mythicCards/blue/blue8.jpg',
     //   cardFace: blueCardsAssets.blue8,
       difficulty: 'hard',
       color:'blue'
     },
     {
       id: 'blue9',
-      url: '../assets/mythicCards/blue/blue9.jpg',
+      url: 'assets/mythicCards/blue/blue9.jpg',
     //   cardFace: blueCardsAssets.blue9,
       difficulty: 'normal',
       color:'blue'
     },
     {
       id: 'blue10',
-      url: '../assets/mythicCards/blue/blue10.jpg',
+      url: 'assets/mythicCards/blue/blue10.jpg',
     //   cardFace: blueCardsAssets.blue10,
       difficulty: 'easy',
       color:'blue'
     },
     {
       id: 'blue11',
-      url: '../assets/mythicCards/blue/blue11.jpg',
+      url: 'assets/mythicCards/blue/blue11.jpg',
     //   cardFace: blueCardsAssets.blue11,
       difficulty: 'normal',
       color:'blue'
     },
     {
       id: 'blue12',
-      url: '../assets/mythicCards/blue/blue12.jpg',
+      url: 'assets/mythicCards/blue/blue12.jpg',
     //   cardFace: blueCardsAssets.blue12,
       difficulty: 'normal',
       color:'blue'
--- js/green.js
+++ js/green.js
@@ -1,126 +1,126 @@
 const cardsDataGreen = [
     {
       id: 'green1',
-      url: '../../assets/mythicCards/green/green1.jpg',
+      url: '../../../assets/mythicCards/green/green1.jpg',
     //   cardFace: greenCardsAssets.green1,
       difficulty: 'easy',
       color:'green'
     },
     {
       id: 'green2',
-      url: '../../assets/mythicCards/green/green2.jpg',
+      url: '../../../assets/mythicCards/green/green2.jpg',
     //   cardFace: greenCardsAssets.green2,
       difficulty: 'hard',
       color:'green'
     },
     {
       id: 'green3',
-      url: '../../assets/mythicCards/green/green3.jpg',
+      url: '../../../assets/mythicCards/green/green3.jpg',
     //   cardFace: greenCardsAssets.green3,
       difficulty: 'hard',
       color:'green'
     },
     {
       id: 'green4',
-      url: '../../assets/mythicCards/green/green4.jpg',
+      url: '../../../assets/mythicCards/green/green4.jpg',
     //   cardFace: greenCardsAssets.green4,
       difficulty: 'hard',
       color:'green'
     },
     {
       id: 'green5',
-      url: '../../assets/mythicCards/green/green5.jpg',
+      url: '../../../assets/mythicCards/green/green5.jpg',
     //   cardFace: greenCardsAssets.green5,
       difficulty: 'hard',
       color:'green'
     },
     {
       id: 'green6',
-      url: '../../assets/mythicCards/green/green6.jpg',
+      url: '../../../assets/mythicCards/green/green6.jpg',
     //   cardFace: greenCardsAssets.green6,
       difficulty: 'hard',
       color:'green'
     },
     {
       id: 'green7',
-      url: '../../assets/mythicCards/green/green7.jpg',
+      url: '../../../assets/mythicCards/green/green7.jpg',
     //   cardFace: greenCardsAssets.green7,
       difficulty: 'normal',
       color:'green'
     },
     {
       id: 'green8',
-      url: '../../assets/mythicCards/green/green8.jpg',
+      url: '../../../assets/mythicCards/green/green8.jpg',
     //   cardFace: greenCardsAssets.green8,
       difficulty: 'normal',
       color:'green'
     },
     {
       id: 'green9',
-      url: '../../assets/mythicCards/green/green9.jpg',
+      url: '../../../assets/mythicCards/green/green9.jpg',
     //   cardFace: greenCardsAssets.green9,
       difficulty: 'normal',
       color:'green'
     },
     {
       id: 'green10',
-      url: '../../assets/mythicCards/green/green10.jpg',
+      url: '../../../assets/mythicCards/green/green10.jpg',
     //   cardFace: greenCardsAssets.green10,
       difficulty: 'normal',
       color:'green'
     },
     {
       id: 'green11',
-      url: '../../assets/mythicCards/green/green11.jpg',
+      url: '../../../assets/mythicCards/green/green11.jpg',
     //   cardFace: greenCardsAssets.green11,
       difficulty: 'normal',
       color:'green'
     },
     {
       id: 'green12',
-      url: '../../assets/mythicCards/green/green12.jpg',
+      url: '../../../assets/mythicCards/green/green12.jpg',
     //   cardFace: greenCardsAssets.green12,
       difficulty: 'easy',
       color:'green'
     },
     {
       id: 'green13',
-      url: '../../assets/mythicCards/green/green13.jpg',
+      url: '../../../assets/mythicCards/green/green13.jpg',
     //   cardFace: greenCardsAssets.green13,
       difficulty: 'normal',
       color:'green'
     },
     {
       id: 'green14',
-      url: '../../assets/mythicCards/green/green14.jpg',
+      url: '../../../assets/mythicCards/green/green14.jpg',
     //   cardFace: greenCardsAssets.green14,
       difficulty: 'normal',
       color:'green'
     },
     {
       id: 'green15',
-      url: '../../assets/mythicCards/green/green15.jpg',
+      url: '../../../assets/mythicCards/green/green15.jpg',
     //   cardFace: greenCardsAssets.green15,
       difficulty: 'normal',
       color:'green'
     },
     {
       id: 'green16',
-      url: '../../assets/mythicCards/green/green16.jpg',
+      url: '../../../assets/mythicCards/green/green16.jpg',
     //   cardFace: greenCardsAssets.green16,
       difficulty: 'easy',
       color:'green'
     },
     {
       id: 'green17',
-      url: '../../assets/mythicCards/green/green17.jpg',
+      url: '../../../assets/mythicCards/green/green17.jpg',
     //   cardFace: greenCardsAssets.green17,
       difficulty: 'easy',
       color:'green'
     },
     {
       id: 'green18',
-      url: '../../assets/mythicCards/green/green18.jpg',
+      url: '../../../assets/mythicCards/green/green18.jpg',
     //   cardFace: greenCardsAssets.green18,
       difficulty: 'easy',
       color:'green'
